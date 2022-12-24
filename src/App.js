e546f65025ab260f2811679da544aa262dfb0728
Sricharan Vodnala <vodnalasricharan@ibm.com>
2022-12-24 08:25:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  About,
+  AuthWrapper,
+  Cart,
+  Error,
+  PrivateRoute,
+  Checkout,
+  Products,
+  Singleproduct,
+} from "./pages";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route exact path="/about">
+          <About></About>
+        </Route>
+        <Route exact path="/cart">
+          <Cart></Cart>
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<Singleproduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import PrivateRoute from "./PrivateRoute";
+import Checkout from "./CheckoutPage";
+import Products from "./ProductsPage";
+import Singleproduct from "./SingleProductPage";
+export {
+  Home,
+  About,
+  AuthWrapper,
+  Cart,
+  Error,
+  PrivateRoute,
+  Checkout,
+  Products,
+  Singleproduct,
+};
